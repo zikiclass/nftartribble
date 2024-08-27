7b783d67ec2e5dd1b6836c1d48ee5b9bb5039469
zikiclass <ezekieledafe5@gmail.com>
2024-08-27 03:51:20 -0700

    adding livechat

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,9 @@
+"use client";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { name, title } from "../../env";
 import AuthProvider from "./auth/Provider";
+import { useEffect } from "react";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "700"],
@@ -10,18 +12,26 @@ const poppins = Poppins({
   display: "swap",
 });
 
-export const metadata = {
-  title: name + title,
-  description: title,
-};
-
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "https://www.smartsuppchat.com/loader.js?";
+    script.async = true;
+    script.onload = () => {
+      window._smartsupp = window._smartsupp || {};
+      window._smartsupp.key = "8dc40739dd488afea819d6f8e1ce69f27fd52230";
+    };
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content={metadata.description} />
-        <meta name="robots" content="index, follow" />
         <link rel="icon" href="/logo.png" />
       </head>
       <body className={poppins.className}>
